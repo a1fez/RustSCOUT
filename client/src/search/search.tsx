cd1cfe73e642ab557280fd1424963ea36b5e4f35
Кирилл <a1fezfora1fez@gmail.com>
2026-08-02 00:14:38 +0100

    search list

--- client/src/search/search.tsx
+++ client/src/search/search.tsx
@@ -9,7 +9,20 @@ interface SearchProps {
 
 const Search = ({ onSearch }: SearchProps) => {
     const [text, setText] = useState('');
-
+    const [server, setServer] = useState('');
+    
+    const servers = [
+  'Rustoria.eu - Main',
+  'Rustoria.eu - Medium',
+  'Rustafied.com - Main',
+  'Rustafied.com - US Medium',
+  'Bloo Lagoon 1.5x',
+  'Stealther 2x',
+  'Vital Rust 10x'
+];
+    
+    
+    
     const handleSubmit = () => {
         const trimmed = text.trim();
         
@@ -35,15 +48,27 @@ const Search = ({ onSearch }: SearchProps) => {
                     onKeyDown={(e) => { if (e.key === 'Enter') handleSubmit(); }} 
                 />
 
-                <button type="button" className="searchButton" onClick={handleSubmit}>
+                <button type="button" className="searchButton" 
+                onClick={handleSubmit}
+                
+                
+                >
                     <HiArrowNarrowRight className="searchIcon"/>
                 </button>
 
                 <input
                     className='searchInput'
                     placeholder='Выберите сервер...'
+                    value={server}
+                    onChange={(e) => {setServer(e.target.value)}}
+                    list='server-list'
                     
                 ></input>
+                <datalist id="server-list">
+                    {servers.map((srv,index) => (
+                        <option key={index} value={srv}/>
+                    ))}
+                </datalist>
             </div>
         </div>
     );
