04777ffaed22d4f21e242913610b60599b80667a
Кирилл <a1fezfora1fez@gmail.com>
2026-08-05 03:26:41 +0100

    updated navbar component

--- client/src/navbar/navbar.tsx
+++ client/src/navbar/navbar.tsx
@@ -1,10 +1,12 @@
 import './navbar.css'
-
+import logo from '../resource/rustScout3.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="logo"> Logo </div>
+            <div className="logo"> 
+                <img src={logo} alt="Logo" />
+            </div>
             <ul className="menu">
                 <li>Главная</li>
                 <li>Поиск</li>
