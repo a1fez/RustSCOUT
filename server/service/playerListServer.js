a6a78424fb8ebc1b02e3aaf4be86661752df144c
Кирилл <a1fezfora1fez@gmail.com>
2026-08-07 13:46:10 +0100

    fix all problems, connected and launched prisma, postgresSQL, redis

--- server/service/playerListServer.js
+++ server/service/playerListServer.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 
 const token = process.env.battleMetricsKey;
-const MIRAGE_SERVER_ID = '26375548'; // ID сервера 
+const MIRAGE_SERVER_ID = '15079350'; // ID сервера 
 
 
 async function getPlayerList(serverId) {
